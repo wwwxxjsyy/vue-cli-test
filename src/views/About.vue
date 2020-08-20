d4a45d0df34c307438eb60f02b355bd2c011cd20
宋灏然 <wwwxxjsyy@163.com>
2020-08-20 14:19:15 +0800

    提交

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <svg-icon iconClass="beian" className="className"></svg-icon>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.className {
+  color: black;
+}
+</style>
